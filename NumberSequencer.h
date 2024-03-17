40b961ed0ee436cef3e958f95c31a37670075d20
MaxSaganyuk <maxsaganyuk@gmail.com>
2024-03-17 15:30:13 +0200

    Rewritten everything to evaluate at compile time

--- NumberSequencer.h
+++ NumberSequencer.h
@@ -1,46 +1,288 @@
-#pragma once
-
-#include <string>
+#include <cstdint>
 #include <vector>
+#include <array>
+#include <string>
+#include <type_traits>
+#include <algorithm>
 
-using llint = long long;
+namespace _numberSequencer
+{ 
 
-class NumberSequencer final {
-private:
-	enum class SequenceType {
-		LINEAR,
-		RECURSIVE
-	};
-
-	static bool CheckForCorrectFormat(const std::string& sequenceString);
-	static std::vector<llint> GetKnownNumbers(const std::string& sequenceString, llint& number, bool& lastNumberMode);
-
-	static void CheckForLinearSequence(const std::vector<llint>& currentDisintSequence, bool& foundPattern);
-	static void CheckForRecursiveSequence(
-		const std::vector<llint>& sequenceToDisint,
-		const std::vector<std::vector<llint>>& disintSequences,
-		bool& foundPattern);
-	static bool DisintegrateSequence(
-		const std::vector<llint>& knownNumbers, 
-		std::vector<std::vector<llint>>& disintSequences,
-		SequenceType& seqType
-	);
-
-	static std::vector<llint> ContinueSequenceLinearly(
-		std::vector<llint>& knownNumbers,
-		std::vector<std::vector<llint>>& disintSequences,
-		llint number,
-		bool lastNumberMode
-	);
-	static void ContinueSequenceRecursivly(std::vector<llint>& knownNumbers);
-
-	static inline bool CheckRules(const std::vector<llint>& continuedSequence, llint number, bool lastNumberMode, size_t counter) {
-		return (lastNumberMode ? continuedSequence.back() : counter) < number;
-	}
-
+template<size_t size>
+class SequenceString
+{
 public:
-	static __declspec(dllexport) std::vector<llint> SequenceDet(const std::string& sequenceString);
-	~NumberSequencer() = delete;
+    constexpr inline SequenceString(const char (&str)[size])
+    {
+        std::copy_n(str, size, value);
+    }
+
+    char value[size];
 };
 
-__declspec(dllexport) std::vector<llint> operator""_nseq(const char* sequenceString, std::size_t);
+
+class NumberSequencer final
+{
+    template<int size>
+    using DisintegratedSequences = std::array<std::array<std::int64_t, size>, size - 1>;
+
+    template<int size>
+    class KnownNumbersStruct
+    {
+    public:
+        constexpr inline KnownNumbersStruct()
+        {
+            number = 0;
+            lastNumberMode = true;
+            knownNumbers = std::array<std::int64_t, size>();
+        }
+
+        std::int64_t number;
+        bool lastNumberMode;
+        std::array<std::int64_t, size> knownNumbers;
+    };
+
+    constexpr static inline int GetDigit(char c)
+    {
+        return c >= '0' && c <= '9' ? c - '0' : -1;
+    }
+
+    template<int size, int resultingSize>
+    constexpr static inline bool CheckRules(
+        const KnownNumbersStruct<size>& knownNumbersStruct,
+        const std::array<std::int64_t, resultingSize>& continuedSequence,
+        size_t continuedSequenceCounter,
+        size_t counter
+    )
+    {
+        return ((knownNumbersStruct.lastNumberMode ? 
+            continuedSequence[continuedSequenceCounter] : counter++) < knownNumbersStruct.number);
+    }
+
+    constexpr static int Atoi(const std::string& str)
+    {
+        int res = 0;
+        int tens = 1;
+
+        for (int i = str.size() - 1; i >= 0; --i)
+        {
+            res += (GetDigit(str[i]) * tens);
+            tens *= 10;
+        }
+
+        return res;
+    }
+
+    constexpr static bool CheckForCorrectFormat(const char* sequenceString)
+    {
+        std::string acceptableSymbols = "01234567890.() ";
+        bool resBool = true;
+
+        for (int i = 0; i < sequenceString[i] != '\0'; ++i)
+        {
+            for (auto& acceptableSymbol : acceptableSymbols)
+            {
+                resBool = false;
+                if (acceptableSymbol == sequenceString[i])
+                {
+                    resBool = true;
+                    break;
+                }
+            }
+            if (!resBool) break;
+        }
+
+        return resBool;
+    }
+
+    constexpr static int GetKnownNumberAmount(const char* sequenceString)
+    {
+        int countNumbers = 0;
+        int countDigitAmount = 0;
+
+        for (int i = 0; sequenceString[i] != '\0'; ++i)
+        {
+            if (GetDigit(sequenceString[i]) != -1)
+            {
+                ++countDigitAmount;
+            }
+            else if (countDigitAmount)
+            {
+                ++countNumbers;
+                countDigitAmount = 0;
+            }
+        }
+
+        return --countNumbers;
+    }
+
+    template<SequenceString seqStr>
+    constexpr static auto GetKnownNumbers()
+    {
+        std::string numberBuffer = "";
+        bool collectSequenceNumbers = true;
+
+        KnownNumbersStruct<GetKnownNumberAmount(seqStr.value)> knownNumbersStruct;
+
+        int count = 0;
+
+        for (int i = 0; seqStr.value[i] != '\0'; ++i)
+        {
+            if (knownNumbersStruct.lastNumberMode && seqStr.value[i] == '(')
+            {
+                knownNumbersStruct.lastNumberMode = false;
+            }
+            else if (seqStr.value[i] == ')') break;
+
+            if (seqStr.value[i] == '.')
+            {
+                collectSequenceNumbers = false;
+            }
+            else if (GetDigit(seqStr.value[i]) != -1)
+            {
+                numberBuffer += seqStr.value[i];
+            }
+            else if (numberBuffer.length() > 0)
+            {
+                knownNumbersStruct.knownNumbers[count++] = Atoi(numberBuffer);
+                numberBuffer = "";
+            }
+        }
+
+        knownNumbersStruct.number = Atoi(numberBuffer);
+
+        return knownNumbersStruct;
+    }
+
+    template<int size>
+    constexpr static bool CheckForLinearSequence(const std::array<std::int64_t, size>& currentDisintSequence)
+    {
+        for (int i = 0; i < currentDisintSequence.size(); ++i)
+        {
+            if (currentDisintSequence[i] < -1 || currentDisintSequence[i] > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    template<int size>
+    constexpr static auto DisintegrateSequence(const std::array<std::int64_t, size>& knownNumbers)
+    {
+        DisintegratedSequences<size> disintSequences;
+        std::array<std::int64_t, size> sequenceToDisint = knownNumbers;
+        std::array<std::int64_t, size> currentDisintSequence;
+        bool foundPattern = true;
+
+        for (int i = 0; i < disintSequences.size(); ++i)
+        {
+            disintSequences[i].fill(-1);
+        }
+
+        for (int i = 0; i < knownNumbers.size() - 1; ++i)
+        {
+            currentDisintSequence.fill(-1);
+            for (int j = 0; j + i + 1 < sequenceToDisint.size(); ++j)
+            {
+                currentDisintSequence[j] = sequenceToDisint[j + 1] - sequenceToDisint[j];
+            }
+
+            disintSequences[i] = currentDisintSequence;
+
+            foundPattern = CheckForLinearSequence<currentDisintSequence.size()>(currentDisintSequence);
+            if (foundPattern) break;
+
+            sequenceToDisint = currentDisintSequence;
+        }
+
+        if (!foundPattern)
+        {
+            disintSequences[0][0] = -1;
+        }
+
+        return disintSequences;
+    }
+
+    template<int size, int resultingSize>
+    constexpr static auto ContinueSequenceLinearly(
+        const KnownNumbersStruct<size>& knownNumbersStruct,
+        const DisintegratedSequences<size>& disintSequences
+    )
+    {
+        DisintegratedSequences<size> reversedDisintSequences;
+
+        for (int i = 0; i < disintSequences.size(); ++i)
+        {
+            reversedDisintSequences[i] = disintSequences[disintSequences.size() - i - 1];
+        }
+
+        std::array<std::int64_t, resultingSize> continuedSequence;
+
+        for (int i = 0; i < knownNumbersStruct.knownNumbers.size(); ++i)
+        {
+            continuedSequence[i] = knownNumbersStruct.knownNumbers[i];
+        }
+
+        std::array<std::int64_t, size - 1> lastNumbers;
+
+        for (int i = 0; i < lastNumbers.size(); ++i)
+        {
+            // Use last existing value of every disintegrated sequence
+            lastNumbers[i] = reversedDisintSequences[i][i] == -1 ? 0 : reversedDisintSequences[i][i];
+        }
+
+        size_t counter = 0;
+        size_t continuedSequenceCounter = knownNumbersStruct.knownNumbers.size() - 1;
+        while (CheckRules<size, resultingSize>(knownNumbersStruct, continuedSequence, continuedSequenceCounter, counter++))
+        {
+            for (size_t i = 0; i < disintSequences.size() - 1; ++i)
+            {
+                lastNumbers[i + 1] += lastNumbers[i];
+            }
+            continuedSequence[continuedSequenceCounter + 1] = (continuedSequence[continuedSequenceCounter] + lastNumbers.back());
+
+            ++continuedSequenceCounter;
+        }
+
+        return continuedSequence;
+    }
+
+public:
+    template<SequenceString seqStr>
+    constexpr static auto SequenceDet()
+    {
+        constexpr bool isFormatCorrect = CheckForCorrectFormat(seqStr.value);
+        static_assert(isFormatCorrect, "Check for correct format failed");
+
+        constexpr auto knownNumbersStruct = GetKnownNumbers<seqStr>();
+
+        constexpr auto disintSequence = DisintegrateSequence<
+            knownNumbersStruct.knownNumbers.size()>
+            (
+                knownNumbersStruct.knownNumbers
+            );
+        static_assert(disintSequence[0][0] != -1, "Cannot disintegrate sequence\n");
+
+        constexpr auto continuedSequence = ContinueSequenceLinearly<
+            knownNumbersStruct.knownNumbers.size(),
+            knownNumbersStruct.knownNumbers.size() + knownNumbersStruct.number>
+            (
+                knownNumbersStruct, disintSequence
+            );
+
+        return continuedSequence;
+    }
+
+    ~NumberSequencer() = delete;
+
+};
+
+};
+
+template<_numberSequencer::SequenceString seqStr>
+constexpr inline auto operator""_nseq()
+{
+    return _numberSequencer::NumberSequencer::SequenceDet<seqStr>();
+}
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
+# NumberSequencer
 
+NumberSequencer is a utility that can be used to simplify creation of arrays filled with growing sequences of numbers at **compile time**. Requires C++20
+
+## Usage
+Include the header and use user defined literal provided `nseq` like so 
+
+```cpp
+constexpr auto seq = "1 2 3 ... (10)"_nseq;
+```
+Input known numbers `"1 2 3"`, separator `"."` (one is enough, three is used to make it look more similar to math notation) and the amount of unknown numbers to evaluate `"(10)"`
+Return type is `std::array<std::int64_t, size>` where `size` will equal the amount of known numbers + unknown numbers (3 + 10). This can be evaluated automatically with `auto`
+The return type should be declared as `constexpr` to process every step at compile time
+
+
+
+
+Result:
+```cpp
+{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+```
+
+You can determine complex sequences without information about the nature of the sequence itself, here's squared number sequence described in first terms
+
+```cpp
+constexpr auto seq = "1 4 9 16 ... (10)"_nseq;
+```
+
+Result:
+```cpp
+{1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196};
+```
+
+This sequence is more complicated, definition for it is a sum of squared and cubed numbers for each n
+
+```cpp
+constexpr auto seq = "2 12 36 80 150 ... (10)"_nseq;
+```
+
+Result:
+```cpp
+{2, 12, 36, 80, 150, 252, 392, 576, 810, 1100, 1452, 1872, 2366, 2940, 3600};
+```
+
+## Notes
+- If the sequence is not growing constantly, algorithm will fail, so `"1 0 1 0 1"` will fail
+- If the sequence referances itself, algorithm will fail, so `"0 1 1 2 3 5 7"` (Fibbonaci sequence) will fail. Might be fixed later
